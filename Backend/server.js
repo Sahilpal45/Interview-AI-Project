85f996f10a9d121b7bac122e21d176ea613bfa0c
Sahilpal45 <spal67277@gmail.com>
2026-03-14 01:33:42 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 const PORT = process.env.PORT || 3000;
-
+console.log("Mongo URI:", process.env.MONGO_URI);
 app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
