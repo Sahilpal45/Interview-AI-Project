c92f15b75afbdc073340800721ee24e71fca1ef0
Sahilpal45 <spal67277@gmail.com>
2026-03-14 02:58:07 +0530

    Update ai.service.js

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -99,7 +99,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                     `
 
     const response = await ai.models.generateContent({
-        model: "gemini-1.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
